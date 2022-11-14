07c38cdc7866a23990732bb74518df9f512ef350
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-14 12:19:06 +0330

    add option color

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,38 @@
+import { Button, Grid } from '@mui/material';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addCardItem, updateCardItem } from '../redux/reducer/Reducer';
-import styles from './Form.module.scss'
+
 
 function Form({ update, setUpdate, FormStatus, setFormStatus }) {
 
     const dispatch = useDispatch()
+    const colors = [
+        {
+            colorname: '#937DC2'
+        },
+        {
+            colorname: '#C689C6'
+        },
+        {
+            colorname: '#32CD32'
+        },
+        {
+            colorname: '#FF4500'
+        },
+        {
+            colorname: '#9ACD32'
+        },
+        {
+            colorname: '#EE82EE'
+        },
+        {
+            colorname: '#FFFF00'
+        },
+        {
+            colorname: '#808000'
+        },
+    ]
     const handleChange = e => {
         setFormStatus({ ...FormStatus, [e.target.name]: e.target.value })
     }
@@ -19,23 +46,32 @@ function Form({ update, setUpdate, FormStatus, setFormStatus }) {
         }
         setFormStatus({
             id: Math.floor(Math.random() * 1000),
-            name: '',
-            lastName: '',
+            title: '',
+            description: '',
         })
         setUpdate('add')
     }
+    const handleColor = (colorName) => {
+        setFormStatus({ ...FormStatus, color: colorName })
+    }
     return (
-        <div>
-            <form onSubmit={handleSubmit} className={styles.container}>
-                <div className={styles.form}>
-                    <input type="text" name='name' value={FormStatus.name} onChange={handleChange} placeholder='name ...' />
-                    <input type="text" name='lastName' value={FormStatus.lastName} onChange={handleChange} placeholder='lastName ...' />
-                    <button type='submit' className='btn btn-success button'>
+        <Grid>
+            <form onSubmit={handleSubmit}>
+                <Grid>
+                    <input type="text" name='title' value={FormStatus.title} onChange={handleChange} placeholder='title ...' />
+                    <input type="text" name='description' value={FormStatus.description} onChange={handleChange} placeholder='description ...' />
+                    {colors.map(color => (
+                        <Grid>
+                            {/* <h5>{color.name}</h5> */}
+                            <Grid style={{ width: "30px", height: '30px', borderRadius: '50%', background: color.colorname, border: FormStatus.color === color.colorname ? '1px solid #000' : 'unset' }} onClick={() => handleColor(color.colorname)} />
+                        </Grid>
+                    ))}
+                    <Button variant='contained' type='submit'>
                         {update === 'add' ? 'add' : 'Update'}
-                    </button>
-                </div>
+                    </Button>
+                </Grid>
             </form>
-        </div>
+        </Grid>
     );
 }
 
