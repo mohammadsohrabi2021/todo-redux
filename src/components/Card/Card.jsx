9a1f480201cad8970cd8b238929b0f115d4e80cb
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-15 13:36:10 +0330

    change style cart

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,6 +4,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteCardItem, checkCardItem } from '../redux/reducer/Reducer';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/material/IconButton'
+import './CardStyle.css'
 function Card({ setUpdate, setFormStatus }) {
     const DataTodos = useSelector(state => state.ToDo)
     const dispatch = useDispatch()
@@ -24,32 +28,41 @@ function Card({ setUpdate, setFormStatus }) {
     }
 
     return (
-        <Grid>
-            <Grid>
-                <input value={search} onChange={handleSearch} placeholder='Search New Todo...' />
+        <Grid container item dir={'rtl'}>
+            <Grid item display={'flex'} sx={{ border: "1px solid #8888 ", borderRadius: "10px" }} xs={12} mb={2}>
+                <IconButton type="button" sx={{ p: "11px" }} aria-label="جستجو">
+                    <SearchIcon />
+                </IconButton>
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    inputProps={{ "aria-label": "search google maps" }}
+                    onChange={handleSearch}
+                    placeholder='جستجوی عبارت مورد نظر ...'
+                />
             </Grid>
             {DataTodos.filter(todo => todo.title.toUpperCase().includes(search.toUpperCase())).map(todo =>
-                <Grid key={todo.id}>
-                    <Grid style={{background:todo.color,opacity:todo.compeleted? '0.5': 'unset'}} >
-                        <Typography variant='h4'>{todo.id}</Typography>
-                        <Typography variant='h4'>{todo.title}</Typography>
-                        <Typography variant='h4'>{todo.description}</Typography>
-                        <Typography variant='h4' onClick={() => handelCheck(todo.id)} style={{ cursor: 'pointer' }}>
-                            {todo.state ? 'yes' :'no'}
-                        </Typography>
+                <Grid key={todo.id}  borderRadius={4} mx={0.5} mb={2} justifyContent={"space-between"} display={"flex"} padding={"16px"} sx={{ background: todo.color }}>
+                    <Grid >
+                        <Grid item display={"flex"} justifyContent={"center"} alignItems={"center"} >
+                            <Grid>
+                                <input type={'radio'} checked={todo.state} onClick={() => handelCheck(todo.id)} />
+                            </Grid>
+                            <Grid mr={2} flexWrap={'wrap'}>
+                                <Typography sx={{ textDecoration: todo.state ? 'line-through' : 'unset' }}>{todo.title}</Typography>
+                                <Typography>{todo.description}</Typography>
+                            </Grid>
+                        </Grid>
                     </Grid>
-
-                    <Grid>
-                        <Button variant='outlined' onClick={() => handleDelet(todo.id)}>
+                    <Grid sx={{ cursor: 'pointer' }}>
+                        <Button variant="outline" onClick={() => handleDelet(todo.id)} sx={{ cursor: 'pointer' }} >
                             <DeleteIcon />
                         </Button>
-                        <Button variant='outlined' onClick={() => handleUpdate(todo)}>
+                        <Button variant="outline" onClick={() => handleUpdate(todo)} sx={{ cursor: 'pointer' }} >
                             <EditIcon />
                         </Button>
                     </Grid>
 
                 </Grid>
-
             )}
         </Grid>
     );
