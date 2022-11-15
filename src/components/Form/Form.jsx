2d95d80385ce35aa05a111759bf62d9ac4f5c655
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-15 13:35:13 +0330

    change style form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,10 @@
-import { Button, Grid } from '@mui/material';
+import { Button, Grid, TextField } from '@mui/material';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addCardItem, updateCardItem } from '../redux/reducer/Reducer';
-
-
+import './Form.mudole.css'
+import BorderColorIcon from '@mui/icons-material/BorderColor'
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 function Form({ update, setUpdate, FormStatus, setFormStatus }) {
 
     const dispatch = useDispatch()
@@ -55,20 +56,35 @@ function Form({ update, setUpdate, FormStatus, setFormStatus }) {
         setFormStatus({ ...FormStatus, color: colorName })
     }
     return (
-        <Grid>
+        <Grid container justifyContent={'center'} alignItems={'center'} dir={'rtl'}>
             <form onSubmit={handleSubmit}>
-                <Grid>
-                    <input type="text" name='title' value={FormStatus.title} onChange={handleChange} placeholder='title ...' />
-                    <input type="text" name='description' value={FormStatus.description} onChange={handleChange} placeholder='description ...' />
-                    {colors.map(color => (
-                        <Grid>
-                            {/* <h5>{color.name}</h5> */}
-                            <Grid style={{ width: "30px", height: '30px', borderRadius: '50%', background: color.colorname, border: FormStatus.color === color.colorname ? '1px solid #000' : 'unset' }} onClick={() => handleColor(color.colorname)} />
-                        </Grid>
-                    ))}
-                    <Button variant='contained' type='submit'>
-                        {update === 'add' ? 'add' : 'Update'}
-                    </Button>
+            <Grid container maxWidth={'600px'} spacing={3} p={2} >
+                    <Grid item xs={12} my={1} display={"flex"} justifyContent={'center'} alignItems={'center'}>
+                        <TextField name={'title'} value={FormStatus.title} onChange={handleChange} required sx={{
+                            width: '80%',
+                           
+                        }} label="موضوع" variant="outlined" />
+                    </Grid>
+                    <Grid item xs={12} my={1} display={"flex"} justifyContent={'center'} alignItems={'center'}>
+                        <TextField name={'description'} value={FormStatus.description} onChange={handleChange} required
+                            sx={{
+                                width: '80%',
+                            }}
+                            label="توضیحات" variant="outlined" />
+                    </Grid>
+                    <Grid xs={12} my={4} display={"flex"} justifyContent={'center'} alignItems={'center'}  flexWrap={'wrap'}  height={'100px'} >
+                        {colors.map(color => (
+                            <Grid p={3}   width={{sm:'100px',xs:'80px',md:'100px',lg:"100px"}} >
+                                <Grid sx={{ width: "30px",cursor:'pointer', height: '30px', borderRadius: '50%', background: color.colorname, border: FormStatus.color === color.colorname ? '1px solid #000' : 'unset' }} onClick={() => handleColor(color.colorname)} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                    <Grid item xs={12} mt={2} mr={6}>
+                        <Button variant='contained' type='submit' xs={12} >
+                            {update === 'add' ?
+                                'اضافه کردن' : "ثبت ویرایش"}
+                        </Button>
+                  </Grid>
                 </Grid>
             </form>
         </Grid>
