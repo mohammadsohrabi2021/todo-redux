2cbefa56f0c21a9164321cad84e6239d5ddc9304
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-11-23 00:51:34 +0330

    add font

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
-import React, { useState } from 'react';
-import { store, persistor } from './components/redux/Store';
-import { Provider } from 'react-redux';
-import Form from './components/Form/Form';
-import Card from './components/Card/Card';
-import { PersistGate } from 'redux-persist/integration/react'
+import React, { useState } from "react";
+import { store, persistor } from "./components/redux/Store";
+import { Provider } from "react-redux";
+import Form from "./components/Form/Form";
+import Card from "./components/Card/Card";
+import { PersistGate } from "redux-persist/integration/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./components/layout/index"
-import './App.css'
-import { Grid } from '@mui/material';
+import Layout from "./components/layout/index";
+import { ThemeProvider } from "@mui/material";
+import theme from "./Theme/theme";
+import "./App.css";
+import { Grid } from "@mui/material";
 function App() {
-  const [update, setUpdate] = useState('add')
+  const [update, setUpdate] = useState("add");
   const [FormStatus, setFormStatus] = useState({
-      id: Math.floor(Math.random() * 1000),
-      title: '',
-      description: '',
-      state: false
-  })
-  
-  const router = createBrowserRouter([
+    id: Math.floor(Math.random() * 1000),
+    title: "",
+    description: "",
+    state: false,
+  });
 
+  const router = createBrowserRouter([
     {
-        path:'/',
-        element:<Layout/>,
-        children:[
-            {
-                path: "/",
-                element: <Card  setUpdate={setUpdate} setFormStatus={setFormStatus} />,
-            },
-            {
-                path: "/Form",
-                element: <Form update={update} setUpdate={setUpdate} FormStatus={FormStatus} setFormStatus={setFormStatus}/>,
-            },  
-             
-        ]
-    }
-    
-]);
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Card setUpdate={setUpdate} setFormStatus={setFormStatus} />,
+        },
+        {
+          path: "/Form",
+          element: (
+            <Form
+              update={update}
+              setUpdate={setUpdate}
+              FormStatus={FormStatus}
+              setFormStatus={setFormStatus}
+            />
+          ),
+        },
+      ],
+    },
+  ]);
   return (
     <Grid>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
+          <ThemeProvider theme={theme}>
+            <RouterProvider router={router} />
+          </ThemeProvider>
         </PersistGate>
       </Provider>
     </Grid>
